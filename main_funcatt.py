56514a0333efa439462f00cb3771925d49dc0f52
hxiaom <447230830@qq.com>
2020-12-23 20:09:51 +0800

    functionality check

--- main_funcatt.py
+++ main_funcatt.py
@@ -69,17 +69,6 @@ def main():
     X_train, y_train, X_test, y_test, word_length, embedding_matrix = data_loader.get_train_data()
     print("X_train\n", X_train)
     print("y_train\n", y_train)
-    
-
-    # load functionality data
-    # print('load sentence functionality data')
-    # func_data_loader = FunctionalityDataLoader(config)
-    # X_func, y_func, word_length_func, embedding_matrix_func = func_data_loader.get_train_data()
-    # print(word_length_func)
-    # print(embedding_matrix_func)
-    # print(type(embedding_matrix_func))
-    # print(embedding_matrix_func.shape)
-    
 
     # load functionality model
     word_length_func = 136411
@@ -88,52 +77,26 @@ def main():
     func_model.load_model()
     print(func_model.model.summary())
 
-    # embedding_layer = Embedding(word_length + 1,
-    #                                 self.config.data_loader.EMBEDDING_DIM,
-    #                                 weights=[embedding_matrix],
-    #                                 input_length=self.config.data_loader.MAX_SENT_LENGTH,
-    #                                 trainable=False
-    #                                 # mask_zero=True  # mask will report ERROR: CUDNN_STATUS_BAD_PARAM
-    #                                 )
-    # sentence_input = Input(shape=(self.config.data_loader.MAX_SENT_LENGTH,), dtype='int32')
-    # x = func_model.layers[-4](embedded_sequences)
-    # x = func_model.layers[-3](x)
-    # x = func_model.layers[-2](x)
-    # y = func_model.layers[-1](x)
-    # func_classification_model = Model(sentence_input, x)
-    # func_classification_model.trainable = False
-    # func_encoder = TimeDistributed(func_classification_model)(review_input) # Query
-
     # create model
     funcatt_model = FuncAttModel(word_length, embedding_matrix, func_model.model, config)
     print(funcatt_model.model.summary())
 
     # train model
-    pseudo_func = np.zeros((2984, 90), dtype='float32')
-    pseudo_func_test = np.zeros((1354, 90), dtype='float32')
-    print("y_train shape", y_train.shape)
-    print("pseudo_func shape", pseudo_func.shape)
-    print("y_test shape", y_test.shape)
-    print("pseudo_func_test shape", pseudo_func_test.shape)
-
-
     funcatt_trainer = FuncAttModelTrainer(funcatt_model.model, [X_train, y_train], [X_test, y_test], config)
     funcatt_trainer.train()
 
+    # create functionality output model
     layer_name = 'func'
-    intermediate_layer_model = Model(inputs=funcatt_trainer.model.input,
-                                       outputs=funcatt_trainer.model.get_layer(layer_name).output)
-    y = func_model.model.layers[-1](intermediate_layer_model.output)
-    func_output = Model(inputs=intermediate_layer_model.input, outputs=y)
-    intermediate_output = func_output.predict(X_test)
+    func_output_model = Model(inputs=funcatt_trainer.model.input,
+                            outputs=funcatt_trainer.model.get_layer(layer_name).output)
+    last_layer = func_model.model.layers[-1](func_output_model.output)
+    func_output = Model(inputs=func_output_model.input, outputs=last_layer)
+    test_output = func_output.predict(X_test)
+    print(np.argmax(test_output[-1], axis=1))
 
-    # print("sample shape", y_test.shape)
-    answer_vec = funcatt_trainer.model.predict(X_test)
-    print(answer_vec[-1])
-    print(np.argmax(answer_vec[-1]))
-    print(intermediate_output[-1])
-    print(np.argmax(intermediate_output[-1], axis=1))
-
+    func_file = open('./experiments/functionality_output.txt', 'w')
+    func_output = np.argmax(test_output, axis=2).tolist()
+    func_file.writelines(["%s\n" % item  for item in a])
 
 if __name__ == '__main__':
     main()
